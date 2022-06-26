03a1444ce92693eee978f2026f6ad5cdee6d4e99
tibu_ <tibu_jfgt@hotmail.com>
2022-06-26 16:51:09 -0500

    Carga de Archivos pdf al expediente digital

--- app/src/main/java/com/example/ingetx/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/ingetx/ui/slideshow/SlideshowFragment.java
@@ -1,18 +1,34 @@
 package com.example.ingetx.ui.slideshow;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.ingetx.Tablero_Alumno;
 import com.example.ingetx.databinding.FragmentSlideshowBinding;
 
 public class SlideshowFragment extends Fragment {
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    ActivityResultLauncher<String> dGetContent;
+    String archivo="";
+
 
     private FragmentSlideshowBinding binding;
 
@@ -24,8 +40,32 @@ public class SlideshowFragment extends Fragment {
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        preferences = getActivity().getSharedPreferences("archivo",0);
+        editor = preferences.edit();
+
         final TextView textView = binding.inicio;
         textView.setText("Agrega los siguientes documentos a tu expediente digital");
+
+        dGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                archivo= result.getPath();
+                Toast.makeText(getActivity(),archivo,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        final Button subir, ver, eliminar;
+        subir = binding.subir;
+        ver = binding.ver;
+        eliminar= binding.eliminar;
+
+
+        subir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dGetContent.launch("application/pdf");
+            }
+        });
 
         return root;
     }
