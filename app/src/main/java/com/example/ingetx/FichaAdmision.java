7433ea822018d2ab7563fe6203bcb47b614ce02c
tibu_ <tibu_jfgt@hotmail.com>
2022-07-05 23:36:58 -0500

    se eliminan comentarios sacados de StackOverflow

--- app/src/main/java/com/example/ingetx/FichaAdmision.java
+++ app/src/main/java/com/example/ingetx/FichaAdmision.java
@@ -169,66 +169,38 @@ public class FichaAdmision extends AppCompatActivity {
     }
 
     private void generatePDF() {
-        // creating an object variable
-        // for our PDF document.
+
         PdfDocument pdfDocument = new PdfDocument();
 
-        // two variables for paint "paint" is used
-        // for drawing shapes and we will use "title"
-        // for adding text in our PDF file.
         Paint paint = new Paint();
         Paint title = new Paint();
 
-        // we are adding page info to our PDF file
-        // in which we will be passing our pageWidth,
-        // pageHeight and number of pages and after that
-        // we are calling it to create our PDF.
         PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pagewidth, pageHeight, 1).create();
 
-        // below line is used for setting
-        // start page for our PDF file.
         PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
 
-        // creating a variable for canvas
-        // from our page of PDF.
         Canvas canvas = myPage.getCanvas();
 
-        // below line is used to draw our image on our PDF file.
-        // the first parameter of our drawbitmap method is
-        // our bitmap
-        // second parameter is position from left
-        // third parameter is position from top and last
-        // one is our variable for paint.
+
+
         canvas.drawBitmap(scaledbmp, 56, 40, paint);
-
-        // below line is used for adding typeface for
-        // our text which we will be adding in our PDF file.
         title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
-
-        // below line is used for setting text size
-        // which we will be displaying in our PDF file.
         title.setTextSize(15);
-
-        // below line is sued for setting color
-        // of our text inside our PDF file.
         title.setColor(ContextCompat.getColor(this, R.color.purple_200));
 
-        // below line is used to draw text in our PDF file.
-        // the first parameter is our text, second parameter
-        // is position from start, third parameter is position from top
-        // and then we are passing our variable of paint which is title.
+
+
         canvas.drawText(username, 209, 60, title);
         canvas.drawText("Instituto Tecnológico Superior de Xalapa", 209, 80, title);
         canvas.drawText("Solicitud de ficha para nuevo ingreso", 209, 100, title);
 
-        // similarly we are creating another text and in this
-        // we are aligning this text to center of our PDF file.
+
+
         title.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
         title.setColor(ContextCompat.getColor(this, R.color.black));
         title.setTextSize(15);
 
-        // below line is used for setting
-        // our text to center of PDF.
+
         title.setTextAlign(Paint.Align.CENTER);
         canvas.drawBitmap(scaledPfp, 296, 300, paint);
         canvas.drawText("Nombre completo: "+last_name+" "+third_name+" "+name, 396, 560, title);
@@ -237,31 +209,18 @@ public class FichaAdmision extends AppCompatActivity {
         canvas.drawText("Carrera: "+selec_carrera, 396, 620, title);
         canvas.drawText("Modalidad: "+selec_mod, 396, 640, title);
 
-        // after adding all attributes to our
-        // PDF file we will be finishing our page.
+
         pdfDocument.finishPage(myPage);
 
-        // below line is used to set the name of
-        // our PDF file and its path.
         File file = new File(Environment.getExternalStorageDirectory(), username+"_ficha.pdf");
 
-
         try {
-            // after creating a file name we will
-            // write our PDF file to that location.
             pdfDocument.writeTo(new FileOutputStream(file));
-
-            // below line is to print toast message
-            // on completion of PDF generation.
             Toast.makeText(FichaAdmision.this, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
-            // below line is used
-            // to handle error
             e.printStackTrace();
             Toast.makeText(FichaAdmision.this, e.toString(),Toast.LENGTH_SHORT).show();
         }
-        // after storing our pdf to that
-        // location we are closing our PDF file.
         pdfDocument.close();
     }
 
@@ -283,8 +242,6 @@ public class FichaAdmision extends AppCompatActivity {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0) {
 
-                // after requesting permissions we are showing
-                // users a toast message of permission granted.
                 boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                 boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
 
