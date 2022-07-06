1e1411eadfbf26b8cfecd2c4f24abb6695fdc05b
tibu_ <tibu_jfgt@hotmail.com>
2022-07-05 23:55:55 -0500

    se eliminan comentarios sacados de StackOverflow

--- app/src/main/java/com/example/ingetx/VerPdf.java
+++ app/src/main/java/com/example/ingetx/VerPdf.java
@@ -25,44 +25,11 @@ import java.io.IOException;
 
 public class VerPdf extends AppCompatActivity {
     PDFView doc;
-    SharedPreferences preferences;
-    SharedPreferences cargar;
-    JSONObject alumno= null;
-    JSONObject usuario = null;
-    String objeto="";
-    String token = "";
-    String name= "";
-    String  last_name= "";
-    String third_name= "";
-    String username= "";
-    String  message= "";
-    String correo= "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_pdf);
-
-        preferences = getSharedPreferences("subida",0);
-        cargar = getSharedPreferences("preferencias",0);
-
-        objeto = this.cargar.getString("jason","");
-
-        try {
-            alumno = new JSONObject(objeto);
-            usuario = alumno.getJSONObject("user");
-            token = alumno.getString("token");
-            name = usuario.getString("name");
-            last_name = usuario.getString("last_name");
-            third_name = usuario.getString("third_name");
-            username = usuario.getString("username");
-            message = alumno.getString("message");
-            correo = usuario.getString("email");
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
 
         String archivo = getIntent().getStringExtra("leer");
         Uri u = Uri.parse(archivo);
